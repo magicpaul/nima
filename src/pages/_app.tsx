aa79f8cf9f63629d48115f206c93b74eba1299b8
magicpaul <itspaulgrant@gmail.com>
2023-09-15 01:41:44 +0100

    css fixd?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import 'css/tailwind.css';
+import 'css/yt-lite.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
