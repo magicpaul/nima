3672d48a70e3338f222e9d205926b3776dd3715b
magicpaul <itspaulgrant@gmail.com>
2023-09-15 18:51:20 +0100

    csssss?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 
-import './css/yt-lite.css';
-import './css/tailwind.css';
+import '../css/yt-lite.css';
+import '../css/tailwind.css';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
