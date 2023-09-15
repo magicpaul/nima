1d323a7a50a170dbd148dbfb796786ccd66e7ebd
magicpaul <itspaulgrant@gmail.com>
2023-09-15 10:35:41 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app';
+
 import 'css/yt-lite.css';
 import 'css/tailwind.css';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+interface CustomPageProps { 
+  
+}
+
+function MyApp({ Component, pageProps }: AppProps<CustomPageProps>) {
+                                            
+  return <Component {...pageProps} />
+                    
 }
