4405005bc3bbe33f35c247fbd0d784a7e2b64130
magicpaul <itspaulgrant@gmail.com>
2025-09-22 23:33:11 +0100

    this better work or so help me...

--- next.config.js
+++ next.config.js
@@ -1,13 +1,27 @@
-module.exports = {
-  webpack: (cfg) => {
-    cfg.module.rules.push({
-      test: /\.md$/,
-      loader: 'frontmatter-markdown-loader',
-    });
-    return cfg;
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'standalone',
   images: {
     domains: ['images.unsplash.com'],
   },
+
+  webpack: (config, { webpack }) => {
+    // Required patch for internal compilation issues with Node globals
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        __dirname: '__dirname',
+        __filename: '__filename',
+      })
+    );
+
+    // Custom markdown loader
+    config.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+    });
+
+    return config;
+  },
 };
+
+module.exports = nextConfig;
