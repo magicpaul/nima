9a34fb9b83c56b2daf79596d8bdaaa46f62afbd7
magicpaul <itspaulgrant@gmail.com>
2025-09-23 00:52:49 +0100

    please

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,7 +6,6 @@ import Layout from 'components/home/Layout';
 import TeamSection from 'components/contact/TeamSection';
 import { ContactAttributes } from 'interfaces/contact';
 import { HomeAttributes } from 'interfaces/home';
-import ContactForm from 'components/contact/ContactForm';
 import Header from 'components/home/Header';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -19,32 +18,6 @@ interface Props {
 const ContactPage: NextPage<Props> = ({ content, homeContent }) => {
   const team = content.attributes;
   const home = homeContent.attributes;
-  const router = useRouter();
-  const confirmationScreenVisible =
-    router.query?.success && router.query.success === 'true';
-  const formVisible = !confirmationScreenVisible;
-
-  const ConfirmationMessage = (
-    <React.Fragment>
-      <div className="container mx-auto">
-        <div className="flex items-center justify-between p-6 border-l-8 sm:py-8 border-green-600 bg-green-500 bg-opacity-10 text-gray-800 shadow-xl my-4">
-          <span>
-            Thank you for submitting this form. Someone should get back to you
-            soon.
-          </span>
-          <button
-            type="button"
-            className="px-8 py-3 font-semibold rounded bg-green-500 text-white"
-            onClick={() =>
-              router.replace('/contact', undefined, { shallow: true })
-            }
-          >
-            Submit Another Response
-          </button>
-        </div>
-      </div>
-    </React.Fragment>
-  );
   return (
     <>
       <SEO />
@@ -59,7 +32,6 @@ const ContactPage: NextPage<Props> = ({ content, homeContent }) => {
           description={team.team_description}
           team={team.team}
         />
-        {formVisible ? <ContactForm /> : ConfirmationMessage}
       </Layout>
     </>
   );
