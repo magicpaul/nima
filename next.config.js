be77a9feabf2a993e00b00ea90f16ea4f46d493f
magicpaul <itspaulgrant@gmail.com>
2025-09-22 22:50:35 +0100

    revert booboos

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-const { getAllContentPaths } = require('./src/lib/content');
-
-// --- Fetch Data for exportPathMap ---
-const eventData = getAllContentPaths('_events');
-const audioData = getAllContentPaths('_audio');
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: 'out',
-  assetPrefix: './',
-  output: 'export',
-  cleanDistDir: true,
-  images: {
-    domains: ['images.unsplash.com'],
-  },
+module.exports = {
   webpack: (cfg) => {
     cfg.module.rules.push({
       test: /\.md$/,
@@ -20,29 +6,11 @@ const nextConfig = {
     });
     return cfg;
   },
-  async exportPathMap(defaultPathMap) {
-    const paths = {
-      ...defaultPathMap,
-    };
-
-    // Generate Paths for Events
-    eventData.forEach((event) => {
-      paths[`/events/${event.slug}`] = {
-        page: '/events/[slug]',
-        query: { slug: event.slug },
-      };
-    });
-
-    // Generate Paths for Audio
-    audioData.forEach((item) => {
-      paths[`/audio/${item.slug}`] = {
-        page: '/audio/[slug]',
-        query: { slug: item.slug },
-      };
-    });
-
-    return paths;
+  images: {
+    domains: ['images.unsplash.com'],
   },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  }
 };
-
-module.exports = nextConfig;
