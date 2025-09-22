a5b7d3aa657330cc8da3090b71925422d5a80cfc
magicpaul <itspaulgrant@gmail.com>
2025-09-23 00:34:42 +0100

    pleaseee

--- next.config.js
+++ next.config.js
@@ -5,32 +5,25 @@ const nextConfig = {
     domains: ['images.unsplash.com'],
   },
 
-  webpack: (config, { webpack, isServer }) => {
+  webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
-      config.node = {
-        __dirname: false,
-      };
       config.resolve.fallback = {
         fs: false,
         path: false,
+        crypto: false,
+      };
+      config.node = {
+        __dirname: false,
+        __filename: false,
       };
     }
 
     if (isServer) {
-      config.externals.push({
-        'next/dist/compiled/cookie': 'commonjs next/dist/compiled/cookie',
-        'next/dist/compiled/gzip-size': 'commonjs next/dist/compiled/gzip-size',
-        'next/dist/compiled/web-vitals/web-vitals':
-          'commonjs next/dist/compiled/web-vitals/web-vitals',
-      });
+      config.externals = [
+        /^(?!\.\/|@|~).+\/next\/dist\/.+$/,
+        ...config.externals,
+      ];
     }
-
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        __dirname: '__dirname',
-        __filename: '__filename',
-      })
-    );
 
     config.module.rules.push({
       test: /\.md$/,
